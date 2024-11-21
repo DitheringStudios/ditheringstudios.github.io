8069b4d4b33db3c3ec3b167dc5ee19f1a2c14c22
A_Generic_Duck <ditheringstudios@gmail.com>
2024-11-21 15:57:55 +0000

    news page + more mobile fixes

--- javascript_my_beloved.js
+++ javascript_my_beloved.js
@@ -1,15 +1,179 @@
-function change_order(post) {
-  const dbParam = JSON.stringify({table:post,limit:10});
-  const xmlhttp = new XMLHttpRequest();
-  xmlhttp.onload = function() {
-    const myObj = JSON.parse(this.responseText);
-    let text = "<table border='1'>"
-    for (let x in myObj) {
-      text += "<tr><td>" + myObj[x].name + "</td></tr>";
-    }
-    text += "</table>"
-    document.getElementById("news").innerHTML = text;
+function updateSize() {
+  let window_width = window.innerWidth;
+  const elementHead = document.getElementById("news")
+  const elements = document.getElementsByClassName("post");
+  const elementsArray = Array.from(elements);
+  if (window_width > 768) {
+    elementsArray.forEach(element => {
+      element.style.float = "none";
+    })
+    elementHead.style.display = "block";
+
+  } else if (window_width > 600) {
+    elementsArray.forEach(element => {
+      element.style.float = "inline-start";
+    })
+    elementHead.style.display = "inline-block";
+
+  } else if (window_width > 300) {
+    elementsArray.forEach(element => {
+      element.style.float = "inline-start";
+    });
+    elementHead.style.display = "inline-block";
   }
-  xmlhttp.open("POST", "posts.json", true);
-  xmlhttp.send("x=" + dbParam);
 }
+
+function change_order(selection) {
+  const elements = document.getElementsByClassName("post");
+  const elementsArray = Array.from(elements);
+
+
+  if (selection === "newest") {
+    elementsArray.forEach(element => {
+      element.remove()
+    });
+    fetch('Posts/posts.json')
+        .then(response => response.json())
+        .then(newsPosts => {
+          const newsFeed = document.getElementById("news");
+
+          newsPosts.forEach(post => {
+            const postElement = document.createElement( "div");
+            postElement.classList.add("post");
+            postElement.style.float = "inline-start";
+
+            // Basic post content
+            let postHTML = `
+          <h2>${post.title}</h2>
+          <p><small>${post.date}</small></p>
+          <p>${post.content}</p>
+        `;
+
+            // Add images dynamically
+            if (post.images > 0) {
+              post.image_url.forEach(image_url => {postHTML += `<img style="height:50px;width:50px;" src="${image_url}" alt=post.imageText>`;
+              });
+            }
+
+            postElement.innerHTML = postHTML;
+            newsFeed.appendChild(postElement)
+            updateSize()
+          });
+        })
+        .catch(error => console.error("Error loading news:", error));
+  }
+
+
+  if (selection === "oldest") {
+    elementsArray.forEach(element => {
+      element.remove()
+    });
+    fetch('Posts/posts.json')
+        .then(response => response.json())
+        .then(newsPosts => {
+          const newsFeed = document.getElementById("news");
+
+          newsPosts.forEach(post => {
+            const postElement = document.createElement( "div");
+            postElement.classList.add("post");
+            postElement.style.float = "inline-start";
+
+            // Basic post content
+            let postHTML = `
+          <h2>${post.title}</h2>
+          <p><small>${post.date}</small></p>
+          <p>${post.content}</p>
+        `;
+
+            // Add images dynamically
+            if (post.images > 0) {
+              post.image_url.forEach(image_url => {postHTML += `<img style="height:50px;width:50px;" src="${image_url}" alt=post.imageText>`;
+              });
+            }
+
+            postElement.innerHTML = postHTML;
+            newsFeed.appendChild(postElement)
+            updateSize()
+          });
+        })
+        .catch(error => console.error("Error loading news:", error));
+  }
+
+
+  if (selection === "game") {
+    elementsArray.forEach(element => {
+      element.remove()
+    });
+    fetch('Posts/posts.json')
+        .then(response => response.json())
+        .then(newsPosts => {
+          const newsFeed = document.getElementById("news");
+
+          newsPosts.forEach(post => {
+            if (post.major === true) {
+              const postElement = document.createElement("div");
+              postElement.classList.add("post");
+              postElement.style.float = "inline-start";
+
+              // Basic post content
+              let postHTML = `
+              <h2>${post.title}</h2>
+              <p><small>${post.date}</small></p>
+              <p>${post.content}</p>
+              `;
+
+              // Add images dynamically
+              if (post.images > 0) {
+                post.image_url.forEach(image_url => {
+                  postHTML += `<img style="height:50px;width:50px;" src="${image_url}" alt=post.imageText>`;
+                });
+              }
+
+              postElement.innerHTML = postHTML;
+              newsFeed.appendChild(postElement)
+              updateSize()
+            }
+            else {
+              postElement = '<h2>no news here at the moment please check back later</h2>>'
+              newsFeed.appendChild(postElement)
+            }
+          });
+        })
+        .catch(error => console.error("Error loading news:", error));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('Posts/posts.json')
+      .then(response => response.json())
+      .then(newsPosts => {
+        const newsFeed = document.getElementById("news");
+
+        newsPosts.forEach(post => {
+          const postElement = document.createElement( "div");
+          postElement.classList.add("post");
+          postElement.style.float = "inline-start";
+
+          // Basic post content
+          let postHTML = `
+          <h2>${post.title}</h2>
+          <p><small>${post.date}</small></p>
+          <p>${post.content}</p>
+        `;
+
+          // Add images dynamically
+          if (post.images > 0) {
+              post.image_url.forEach(image_url => {postHTML += `<img style="height:50px;width:50px;" src="${image_url}" alt=post.imageText>`;
+              });
+          }
+
+          postElement.innerHTML = postHTML;
+          if (newsFeed.hasChildNodes() === false)
+            newsFeed.appendChild(postElement)
+          else
+            newsFeed.insertBefore(postElement, newsFeed.firstChild);
+          updateSize()
+        });
+      })
+      .catch(error => console.error("Error loading news:", error));
+});
