8bdc77d2de15741fd04160d87ddbee57f75c755a
A_Generic_Duck <ditheringstudios@gmail.com>
2024-12-05 09:15:32 +0000

    new way of adding videos+fixed index page

--- javascript_my_beloved.js
+++ javascript_my_beloved.js
@@ -1,28 +1,85 @@
 function updateSize() {
-  let window_width = window.innerWidth;
+    let window_width = window.innerWidth;
+
+    if (document.getElementById("games") !== null) {
+        const elementHeadForGames = document.getElementById("game_releases");
+        const elementsForGames = document.getElementsByClassName("gamePost");
+        const elementsArrayGame = Array.from(elementsForGames);
+        const elementButtonForGame = document.getElementsByClassName("loadMoreButtonForGame");
+        const elementButtonsArrayForGame = Array.from(elementButtonForGame);
+
+        if (window_width > 768) {
+            elementHeadForGames.style.display = "block";
+            elementsArrayGame.forEach(element => {
+                element.style.float = "none";
+            })
+            elementButtonsArrayForGame.forEach(elementButtons => {
+                elementButtons.style.height = "auto";
+            })
+        } else if (window_width > 600) {
+            elementsArrayGame.forEach(element => {
+                element.style.float = "inline-start";
+            })
+            elementHeadForGames.style.display = "inline-flex";
+            elementButtonsArrayForGame.forEach(elementButtons => {
+                elementButtons.style.height = "1300";
+            })
+        } else if (window_width > 300) {
+            elementsArrayGame.forEach(element => {
+                element.style.float = "inline-start";
+            });
+            elementHeadForGames.style.display = "inline-flex";
+            elementButtonsArrayForGame.forEach(elementButtons => {
+                elementButtons.style.height = "1300";
+            })
+        }
+    }
+    if (document.getElementById("projects") !== null) {
+        const elementHeadForGames = document.getElementById("game_releases");
+        const elementsForGames = document.getElementsByClassName("gamePost");
+        const elementsArrayGame = Array.from(elementsForGames);
+        const elementButtonForGame = document.getElementsByClassName("loadMoreButtonForGame");
+        const elementButtonsArrayForGame = Array.from(elementButtonForGame);
+
+        if (window_width > 768) {
+            elementHeadForGames.style.display = "block";
+            elementsArrayGame.forEach(element => {
+                element.style.float = "none";
+            })
+            elementButtonsArrayForGame.forEach(elementButtons => {
+                elementButtons.style.height = "auto";
+            })
+        } else if (window_width > 600) {
+            elementsArrayGame.forEach(element => {
+                element.style.float = "inline-start";
+            })
+            elementHeadForGames.style.display = "inline-flex";
+            elementButtonsArrayForGame.forEach(elementButtons => {
+                elementButtons.style.height = "1300";
+            })
+        } else if (window_width > 300) {
+            elementsArrayGame.forEach(element => {
+                element.style.float = "inline-start";
+            });
+            elementHeadForGames.style.display = "inline-flex";
+            elementButtonsArrayForGame.forEach(elementButtons => {
+                elementButtons.style.height = "1300";
+            })
+        }
+    }
+
   const elementHead = document.getElementById("news");
   const elements = document.getElementsByClassName("post");
-  const elementHeadForGames = document.getElementById("game_releases");
-  const elementsForGames = document.getElementsByClassName("gamePost");
   const elementsArray = Array.from(elements);
-  const elementsArrayGame = Array.from(elementsForGames);
   const elementButton = document.getElementsByClassName("loadMoreButton");
-  const elementButtonForGame = document.getElementsByClassName("loadMoreButtonForGame");
   const elementButtonsArray = Array.from(elementButton);
-  const elementButtonsArrayForGame = Array.from(elementButtonForGame);
+
   if (window_width > 768) {
     elementsArray.forEach(element => {
       element.style.float = "none";
     })
-      elementsArrayGame.forEach(element => {
-          element.style.float = "none";
-    })
     elementHead.style.display = "block";
-    elementHeadForGames.style.display = "block";
     elementButtonsArray.forEach(elementButtons => {
-        elementButtons.style.height = "auto";
-    })
-    elementButtonsArrayForGame.forEach(elementButtons => {
         elementButtons.style.height = "auto";
     })
 
@@ -30,15 +87,8 @@ function updateSize() {
     elementsArray.forEach(element => {
       element.style.float = "inline-start";
     })
-    elementsArrayGame.forEach(element => {
-      element.style.float = "inline-start";
-    })
     elementHead.style.display = "inline-flex";
-    elementHeadForGames.style.display = "inline-flex";
     elementButtonsArray.forEach(elementButtons => {
-        elementButtons.style.height = "1300";
-    })
-    elementButtonsArrayForGame.forEach(elementButtons => {
         elementButtons.style.height = "1300";
     })
 
@@ -46,15 +96,8 @@ function updateSize() {
     elementsArray.forEach(element => {
       element.style.float = "inline-start";
     });
-    elementsArrayGame.forEach(element => {
-      element.style.float = "inline-start";
-    });
     elementHead.style.display = "inline-flex";
-    elementHeadForGames.style.display = "inline-flex";
     elementButtonsArray.forEach(elementButtons => {
-        elementButtons.style.height = "1300";
-    })
-    elementButtonsArrayForGame.forEach(elementButtons => {
         elementButtons.style.height = "1300";
     })
   }
@@ -591,11 +634,9 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(response => response.json())
       .then(newsPosts => {
         const newsFeed = document.getElementById("news");
-        let i = 0;
         let newsAdded = 0;
         newsPosts.forEach(post => {
-            if (i < 5) {
-                i++
+            if (newsAdded < 5) {
                 const postElement = document.createElement("div");
                 postElement.classList.add("post");
                 postElement.style.float = "inline-start";
@@ -1283,4 +1324,50 @@ function change_order_projects(selection) {
             })
             .catch(error => console.error("Error loading news:", error));
     }
+}
+
+function LoadVideos() {
+    fetch('Posts/videos.json')
+        .then(response => response.json())
+        .then(videos => {
+            const videoColum = document.getElementById("videoColum");
+            let videosAdded = 0;
+            videos.forEach(video => {
+                if (videosAdded < 5) {
+                    videosAdded++;
+                    const videoElement = document.createElement("div");
+                    videoElement.id = 'video';
+
+                    videoElement.innerHTML = `
+					<iframe class="media-content-l media-content-m media-content-s"
+							src="${video.url}"
+							width="90%"
+							height="90%"
+							allowfullscreen>
+					</iframe>
+                  `
+
+                    videoColum.appendChild(videoElement);
+                    updateSize();
+                }
+            });
+
+            if (videosAdded === 0) {
+                const noVideos = document.createElement("div");
+                noVideos.classList.add("video");
+                noVideos.innerHTML = '<h2>No videos here at the moment. Please check back later.</h2>';
+                videoColum.appendChild(noVideos);
+            }
+
+            if (videosAdded > 0 && videos.length > 5) {
+                const loadMoreButton = document.createElement("a");
+                loadMoreButton.innerHTML = '<button><buttonText>View More</buttonText></button>';
+                loadMoreButton.style.border = "1px solid";
+                loadMoreButton.style.borderRadius = "10px";
+                loadMoreButton.style.height = "130px";
+                loadMoreButton.href = "https://github.com/Cat5TV/pinecraft";
+                videoColum.appendChild(loadMoreButton);
+            }
+        })
+        .catch(error => console.error("Error loading videos:", error));
 }
